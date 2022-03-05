4f86898cbbb3df7e6021d29a55cad641f156b156
sabadoryo <eabilbay@gmail.com>
2022-03-05 19:00:32 +0600

    fix

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import store from "./store"
 
-const DEV = 'http://selevinia.test';
+// const DEV = 'http://selevinia.test';
 const REMOTE_DEV = 'http://sabadoryo.com';
 
 /*
